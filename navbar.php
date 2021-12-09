200e144047bf44d670add8523d0d73c87e61f9fd
pawer882 <pau.nicolau.fiol@gmail.com>
2021-12-09 22:33:19 +0100

    e

--- navbar.php
+++ navbar.php
@@ -28,7 +28,7 @@
                             $query = "SELECT * from categoria ORDER BY nomCat ASC";
                             $result = mysqli_query($con,$query);
                             while($row = mysqli_fetch_array($result)){
-                                echo '<a class="dropdown-item" href="llistaContingutCat.php?id='".$row['nomCat']."'">'.$row['nomCat'].'</a>';
+                                //echo '<a class="dropdown-item" href="llistaContingutCat.php?id='".$row['nomCat']."'">'.$row['nomCat'].'</a>';
                             }
                         ?>
                             <a class="dropdown-item" href="#">Todas las categorias</a>
