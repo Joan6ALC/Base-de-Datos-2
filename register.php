3500b210eb203e402915ddc6610c23dc12be4d3e
Juan Carlos Bujosa <85440338+jbherreros@users.noreply.github.com>
2021-12-15 20:12:34 +0100

    Update register.php

--- register.php
+++ register.php
@@ -10,14 +10,14 @@
     $date = date('y-m-d'); // obtenim data local
 
     // Comprovam que les contrasenyes introduides coincideixen
-    if($password1!=$password2){ // Si coincideixen, error 1
+    if($password1!=$password2){ // Si no coincideixen, error 1
         header("Location: registerform.php?error=1&name=$name&surname1=$surname1&surname2=$surname2&dob=$dob&username=$username");
         die();
     }
 
     include "connection.php"; // Connexió a bd
 
-    // Comprobam si l'username triat ja està en ús
+    // Comprovam si l'username triat ja està en ús
     $query = 'SELECT username FROM persona WHERE username="'.$username.'"';
     $result=mysqli_query($con, $query);
     $row = mysqli_fetch_array($result);
@@ -28,11 +28,12 @@
         die();
     }
 
+    // Calcula l'edat d'un usuari en el moment actual i li assignam un tipus de contingut que pot veure segons l'edat
     $firstDate  = new DateTime($dob);
     $secondDate = new DateTime($date);
     $intvl = $firstDate->diff($secondDate);
     $edat=$intvl->y;
-    echo $edat;
+
     //echo $intvl->y . " year, " . $intvl->m." months and ".$intvl->d." day"; 
     if($edat<9){
         $tipus=1;
@@ -42,12 +43,13 @@
         $tipus=3;
     }
 
+    // Inserció del nou usuari a la taula persona
     $hash=crypt($password1,"");
     $query = "INSERT INTO persona(dataAlta, username, password, nom, llinatge1, llinatge2, dataNaixament, administrador, IdTipus) VALUES ('".$date."','".$username."', '".$hash."', '".$name."', '".$surname1."', '".$surname2."', '".$dob."', false, $tipus)";
     mysqli_query($con, $query); // Registram el nou usuari
     echo $query;
 
-    //L'usuari s'ha registrat correctament, li permetem iniciar sessió
-    header("Location: index.php?msg=3"); // Redirigim a l'usuari a la pàgina principal
+    // L'usuari s'ha registrat correctament, li permetem iniciar sessió
+    header("Location: index.php?msg=3"); // Redirigim a l'usuari a la pàgina principal amb missatge d'èxit
     die();
 ?>
