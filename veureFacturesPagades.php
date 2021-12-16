8109cf1d76add09ede9b595fdbd3eb56e2f6f127
paulasalom <paulaferrer2000@hotmail.es>
2021-12-16 15:49:29 +0100

    commit

--- veureFacturesPagades.php
+++ veureFacturesPagades.php
@@ -78,13 +78,17 @@
             }
         
         ?>
-        <!--botó per a tornar enrera-->
+        </section>
+
+
+        <section style="padding-left: 10% ;">
+            <!--botó per a tornar enrera-->
             <form action="veureFactures.php" method="post" id="$trob">
                 <button type="submit" class="btn btn-danger">Atrás</button> 
             </form>
-      
-            
         </section>
+
+        
         <!-- Frameworks -->
         <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
