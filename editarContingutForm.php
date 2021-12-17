0cee143f248b6db7d3e7d16d0702f7c90b07f1e1
paulasalom <paulaferrer2000@hotmail.es>
2021-12-17 21:06:36 +0100

    rm <style>

--- editarContingutForm.php
+++ editarContingutForm.php
@@ -78,8 +78,6 @@ $tipus = mysqli_query($con, $resultTipus);
         } ?>
     </header>
     <section>
-
-
         <div class="container">
             <div class="padding"><br></div>
             <div class="row">
@@ -209,15 +207,8 @@ $tipus = mysqli_query($con, $resultTipus);
     </section>
 
     <footer>
-        <div style="color: grey; font-size: 9px">PelisTube &copy; 2021</div>
+        <div>PelisTube &copy; 2021</div>
     </footer>
-
-    <style>
-        body {
-            background-image: url("img/background2.jpg");
-            background-position: absolute;
-        }
-    </style>
     <!-- Frameworks -->
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
