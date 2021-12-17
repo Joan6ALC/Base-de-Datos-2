278d1ce53a2d4d133583de33714c34da79d2e510
Juan Carlos Bujosa <85440338+jbherreros@users.noreply.github.com>
2021-12-17 19:34:45 +0100

    Update login.php

--- login.php
+++ login.php
@@ -269,13 +269,6 @@
             <div style="color: grey; font-size: 9px">PelisTube &copy; 2021</div>
         </footer>
 
-        <style>
-            body {
-                background-image: url("img/background2.jpg");
-                background-position:absolute;
-            }
-        </style>
-
     <!-- Frameworks -->
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
