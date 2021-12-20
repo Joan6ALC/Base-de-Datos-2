52adc85545018b83e1d637cc60900146be1b0c20
Juan Carlos Bujosa <85440338+jbherreros@users.noreply.github.com>
2021-12-20 14:42:58 +0100

    Update carpeta JC

--- BD243220290R/restaurarContingut.php
+++ BD243220290R/restaurarContingut.php
@@ -1,5 +1,4 @@
 <?php session_start();
-
     if(!isset($_SESSION['username'])){
         header("Location: login.php");
         die();
@@ -11,19 +10,27 @@
     }
 
     $IdContingut=$_GET['id'];
-    $Redirect=$_GET['redir'];
+    $Redirect=$_GET['redir']; 
 
     // Connexió a bd
     include "connection.php";
 
-    // Fem invisible el contingut
+    // Miram si la categoría ha estat invisibilitzada o no
+    $query="SELECT categoria.visible FROM contingut JOIN categoria ON contingut.nomCat=categoria.nomCat AND IdContingut=".$IdContingut;
+    $result=mysqli_query($con, $query);
+    $row = mysqli_fetch_array($result);
+
+    // Fem visible el contingut
     $query="UPDATE contingut SET visible=1 WHERE IdContingut=".$IdContingut;
     $result=mysqli_query($con, $query);
 
     mysqli_close($con);
 
-    if (!isset($row['username'])){
-        header("Location: $Redirect?msg=10");
+    if($row['visible']!=1){ // Si no pertany a una categoria visible
+        header("Location: $Redirect?msg=20");
         die();
-    }
+    } 
+    
+    header("Location: $Redirect?msg=10");
+    die();
 ?>
