e71158436e845d0f46b01c581fc3fb818e262a63
AlbertoYang1 <alberto.24yang@gmail.com>
2021-12-21 19:15:38 +0100

    update carpeta

--- BD2X5070928A/eliminarCategoriaFavorit.php
+++ BD2X5070928A/eliminarCategoriaFavorit.php
@@ -10,7 +10,7 @@
 
     $query="DELETE FROM categoriafavorits WHERE nomCat='".$Categoria."' AND IdContracte='".$_SESSION['IdContracte']."'";
     $result=mysqli_query($con, $query);
-    header("Location: $redirect?msg=6");
+    header("Location: $redirect?msg=6&id=$Categoria");
 
     mysqli_close($con);
 ?>
