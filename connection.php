715bb84357b20565d277872f4cf069c8cdf98f1c
Juan Carlos Bujosa <85440338+jbherreros@users.noreply.github.com>
2021-12-22 20:39:46 +0100

    Update

--- connection.php
+++ connection.php
@@ -1,5 +1,5 @@
 <?php
 $con = mysqli_connect("localhost","root","") or die("Localhost no disponible");
-$db = mysqli_select_db($con,"pelistube") or die("Base de datos no disponible");
+$db = mysqli_select_db($con,"BD211") or die("Base de datos no disponible");
 $con -> set_charset("utf8");
 ?>
